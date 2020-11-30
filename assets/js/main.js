11bd1d99943e8af3b820d1771d78df5b7c9ced2e
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-30 17:10:49 +0100

    Aggiunta una funzione js che al passare del mouse fa comparire i menu
    Aggiunta una funzione js che al passare del mouse fa comparire i menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
 $(function (){
+    //variabile dei diversi menu atendina
+    var menu = $('.menu_titolo');
+
+    menu.mouseenter(function () { 
+        //console.log("cliccato");
+        $(this).show();
+    });
+
+    menu.mouseleave(function (){
+        $(this).hide();
+    });
 });
+
