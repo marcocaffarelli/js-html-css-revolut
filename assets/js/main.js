9de375bfa154d012801976a0e07b05ab3a760cf2
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-30 17:39:07 +0100

    Aggiunta funzione che fa apparire menu a tendina
    Aggiunta funzione che fa apparire menu a tendina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,60 @@
 $(function (){
-    //variabile dei diversi menu atendina
-    var menu = $('.menu_titolo');
+    // //variabile dei diversi menu atendina
+    // var menu = $('.menu_titolo');
 
-    menu.mouseenter(function () { 
+    $('.menu_titolo1').mouseenter(function () { 
         //console.log("cliccato");
-        $('.menu_tendina').show();
+        $('.tendina1').show();
     });
 
-    menu.mouseleave(function (){
-        $('.menu_tendina').hide();
+    $('.menu_titolo1').mouseleave(function (){
+        $('.tendina1').hide();
     });
+
+    $('.menu_titolo2').mouseenter(function () { 
+        //console.log("cliccato");
+        $('.tendina2').show();
+    });
+
+    $('.menu_titolo2').mouseleave(function (){
+        $('.tendina2').hide();
+    });
+
+    $('.menu_titolo3').mouseenter(function () { 
+        //console.log("cliccato");
+        $('.tendina3').show();
+    });
+
+    $('.menu_titolo3').mouseleave(function (){
+        $('.tendina3').hide();
+    });
+
+    $('.menu_titolo4').mouseenter(function () { 
+        //console.log("cliccato");
+        $('.tendina4').show();
+    });
+
+    $('.menu_titolo4').mouseleave(function (){
+        $('.tendina4').hide();
+    });
+
+    $('.menu_titolo5').mouseenter(function () { 
+        //console.log("cliccato");
+        $('.tendina5').show();
+    });
+
+    $('.menu_titolo5').mouseleave(function (){
+        $('.tendina5').hide();
+    });
+
+    $('.menu_titolo6').mouseenter(function () { 
+        //console.log("cliccato");
+        $('.tendina6').show();
+    });
+
+    $('.menu_titolo6').mouseleave(function (){
+        $('.tendina6').hide();
+    });
+
 });
 
